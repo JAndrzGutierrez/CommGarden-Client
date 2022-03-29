20029256db5777b9187686f7c8d0790f7c876b33
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-03-28 20:32:20 -0400

    Created sign-In form

--- src/Components/SignInForm/SignInForm.js
+++ src/Components/SignInForm/SignInForm.js
@@ -52,5 +52,34 @@ const SignInForm = () => {
       <div className="error">{errorMessages.message}</div>
     );
 
-}
+    const Form = (
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <div className="input-container">
+              <label>Username </label>
+              <input type="text" name="uname" required />
+              {renderErrorMessage("uname")}
+            </div>
+            <div className="input-container">
+              <label>Password </label>
+              <input type="password" name="pass" required />
+              {renderErrorMessage("pass")}
+            </div>
+            <div className="button-container">
+              <input type="submit" />
+            </div>
+          </form>
+        </div>
+      );
+    
+      return (
+        <div className="sign-in-form">
+          <div className="login-form">
+            <div className="title">Sign In</div>
+            {isSubmitted ? <div>User is successfully logged in</div> : Form}
+          </div>
+        </div>
+      );
+    }
+
 export default SignInForm
