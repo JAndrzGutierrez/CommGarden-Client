6e0504dc2b7b090dc2b0f4c50e18ab6ffee08ec3
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-03-28 20:28:00 -0400

    Error preventing and signin logic

--- src/Components/SignInForm/SignInForm.js
+++ src/Components/SignInForm/SignInForm.js
@@ -1,8 +1,7 @@
 import {useState} from "react"
 
-
 const SignInForm = () => {
-    
+
     // React States
     const [errorMessages, setErrorMessages] = useState({});
     const [IsSubmitted, setIsSubmitted] = useState(false);
@@ -24,5 +23,34 @@ const SignInForm = () => {
     pass: "invalid password"
   };
 
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
 }
-export default SignUp
+export default SignInForm
