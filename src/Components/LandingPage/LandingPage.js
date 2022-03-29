e9b1ef7d91906835d0645ebdaccf30707b02d174
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-03-28 21:01:33 -0400

    Added Sign-In Form

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,11 +1,14 @@
 import NavBar from "../NavBar/NavBar"
+import Container from "react-bootstrap/Container"
+import SignInForm from "../SignInForm/SignInForm"
+
 
 
 const LandingPage = () => {
     return(
         <div className="landing-page">
          <NavBar/>
-        
+         <SignInForm/>
         </div>
     )
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,7 +2,6 @@ import Container from "react-bootstrap/Container"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import NavDropdown from "react-bootstrap/NavDropdown"
-
  
 const NavBar = () =>{
     return(
